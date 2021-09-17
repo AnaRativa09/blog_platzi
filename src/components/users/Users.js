d84b1c3a9f1b994b7b6bd1c53680e955e820a8bf
AnaRativa09 <anarativa09@gmail.com>
2021-09-17 12:13:32 -0500

    Feat: Add router and modularize user view

---
+++ src/components/users/Users.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+class Users extends Component {
+  constructor() {
+    super();
+    this.state = {
+      users: []
+    }
+  }
+
+  async componentDidMount() {
+    const responseData = await axios.get('https://jsonplaceholder.typicode.com/users')
+    this.setState({
+      users: responseData.data
+    })
+  }
+
+  putRows = () => this.state.users.map((user) => (
+    <tr key={user.id}>
+      <td>
+        {user.name}
+      </td>
+      <td>
+        {user.email}
+      </td>
+      <td>
+        {user.website}
+      </td>
+    </tr>
+  ));
+
+  render() {
+    return (
+      <div className="margen">
+        <table className="tabla">
+          <thead>
+            <tr>
+              <th>
+                Name
+              </th>
+              <th>
+                Email
+              </th>
+              <th>
+                Website
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.putRows()}
+          </tbody>
+        </table>
+      </div>
+    )
+  }
+};
+
+export default Users;
