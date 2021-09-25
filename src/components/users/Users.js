b4ef97aef280ed4a3a8d621a85341555b6fd60b7
AnaRativa09 <anarativa09@gmail.com>
2021-09-25 17:57:39 -0500

    Feat: Add spinner to loading state

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -2,10 +2,39 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as usersActions from '../../actions/usersActions';
 
+import Spinner from '../general/Spinner';
+
 class Users extends Component {
   componentDidMount() {
     this.props.getAll();
   }
+
+  putContent = () => {
+    if (this.props.loading) {
+      return <Spinner />;
+    }
+
+    return (
+      <table className="tabla">
+        <thead>
+          <tr>
+            <th>
+              Name
+            </th>
+            <th>
+              Email
+            </th>
+            <th>
+              Website
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.putRows()}
+        </tbody>
+      </table>
+    );
+  };
 
   putRows = () => this.props.users.map((user) => (
     <tr key={user.id}>
@@ -24,24 +53,7 @@ class Users extends Component {
   render() {
     return (
       <div>
-        <table className="tabla">
-          <thead>
-            <tr>
-              <th>
-                Name
-              </th>
-              <th>
-                Email
-              </th>
-              <th>
-                Website
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {this.putRows()}
-          </tbody>
-        </table>
+        {this.putContent()}
       </div>
     );
   }
