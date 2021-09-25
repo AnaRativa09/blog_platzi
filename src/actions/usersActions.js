d1225663e881040d14f601936bd1b6f7c32e97a2
AnaRativa09 <anarativa09@gmail.com>
2021-09-25 17:58:12 -0500

    feat: add action types (loading, error)

--- src/actions/usersActions.js
+++ src/actions/usersActions.js
@@ -1,12 +1,23 @@
 import axios from 'axios';
+import * as actionTypes from '../types/usersTypes';
 
 export const getAll = () => async (dispatch) => {
-  const responseData = await axios.get('https://jsonplaceholder.typicode.com/users');
-
   dispatch({
-    type: 'get-users',
-    payload: responseData.data,
+    type: actionTypes.LOADING,
   });
+
+  try {
+    const responseData = await axios.get('https://jsonplaceholder.typicode.com/users');
+    dispatch({
+      type: actionTypes.GET_USERS,
+      payload: responseData.data,
+    });
+  } catch (err) {
+    dispatch({
+      type: actionTypes.ERROR,
+      payload: err.message,
+    });
+  }
 };
 
 export const getAll2 = () => async (dispatch) => {
--- src/reducers/usersReducer.js
+++ src/reducers/usersReducer.js
@@ -1,11 +1,25 @@
+import * as actionTypes from '../types/usersTypes';
+
 const INITIAL_STATE = {
   users: [],
+  loading: false,
+  error: '',
 };
 
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case 'get-users':
-      return { ...state, users: action.payload };
+    case actionTypes.GET_USERS:
+      return {
+        ...state,
+        users: action.payload,
+        loading: false,
+      };
+
+    case actionTypes.LOADING:
+      return { ...state, loading: true };
+
+    case actionTypes.ERROR:
+      return { ...state, error: action.payload, loading: false };
 
     default: return state;
   }
